cae5a1a69d0caebcad09794a38eb5d8512912834
mikaelarosasflores <168119485+mikaelarosasflores@users.noreply.github.com>
2024-06-28 03:33:10 -0300

    Update Cuadro.java

--- CATE PRESENTACIÓN/Objetos/src/Muebles/Cuadro.java
+++ CATE PRESENTACIÓN/Objetos/src/Muebles/Cuadro.java
@@ -3,7 +3,7 @@ package Muebles;
 public class Cuadro extends Mueble {
 
 	public Cuadro() {
-		super("Dildo con clavo", 250, 1);
+		super("Cuadro", 250, 1);
 		// TODO Auto-generated constructor stub
 	}
  
